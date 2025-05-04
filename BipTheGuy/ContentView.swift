96101702f4f67387fb3d3e0842009e9f886a3430
Vincent Cina <vcina@me.com>
2025-05-04 15:58:05 -0400

    Lesson 49. Photo picker.

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -7,11 +7,14 @@
 
 import SwiftUI
 import AVFAudio
+import PhotosUI
 
 struct ContentView: View {
     
     @State private var audioPlayer: AVAudioPlayer!
     @State private var isFullSize = true
+    @State private var selectedPhoto: PhotosPickerItem?
+    @State private var bipImage = Image("clown")
     
     
     var body: some View {
@@ -19,27 +22,34 @@ struct ContentView: View {
             
             Spacer()
             
-            Image("clown")
+            bipImage
                 .resizable()
                 .scaledToFit()
                 .scaleEffect(isFullSize ? 1.0 : 0.9)
                 .onTapGesture {
                     playSound(soundName: "punchSound")
                     isFullSize = false
-//                    scale = scale + 0.1 // increase image size by 10%
+                    //                    scale = scale + 0.1 // increase image size by 10%
                     withAnimation (.spring(response: 0.3, dampingFraction: 0.3)) {
                         isFullSize = true // will go from 90% to 100% size using the .spring animation
                     }
                 }
-//                .animation(.spring(response: 0.3, dampingFraction: 0.3), value: scale)
             
             
             Spacer()
             
-            Button {
-                //TODO: Button Action here
-            } label: {
+            PhotosPicker(selection: $selectedPhoto, matching: .images, preferredItemEncoding: .automatic) {
                 Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
+            .onChange(of: selectedPhoto) {
+                Task {
+                    guard let selectedImage = try? await selectedPhoto?.loadTransferable(type: Image.self)
+                    else {
+                        print("😡 ERROR: Could not get Image from loadTransferrable")
+                        return }
+                    bipImage = selectedImage
+                }
+                
             }
             
         }
